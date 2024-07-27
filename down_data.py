520dc87e871f8fcd22e342c1eb58da2c01282935
370025263 <370025263@qq.com>
2024-07-28 02:54:31 +0800

    update_data

---
+++ down_data.py
@@ -0,0 +1,15 @@
+import os
+from datasets import load_dataset
+
+# 指定本地缓存目录
+cache_dir = os.path.join(os.getcwd(), 'data')
+
+# 创建目录（如果不存在）
+os.makedirs(cache_dir, exist_ok=True)
+
+# 加载数据集并指定缓存目录
+dataset = load_dataset('deatos/fineweb-edu-mini-combined', cache_dir=cache_dir)
+
+# 打印数据集信息
+print(dataset)
+
